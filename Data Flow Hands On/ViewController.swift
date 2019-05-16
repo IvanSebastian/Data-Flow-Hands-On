94e03dc45afdcc3c230aefb5c4be6eb6c4323b45
Ivan Sebastian <ivansedbert@gmail.com>
2019-05-16 21:29:37 +0700

    add UI Alert Controller and UI Alert Action

--- Data Flow Hands On/ViewController.swift
+++ Data Flow Hands On/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   
+        if fullNameTextField.text == "" || phoneTextField.text == "" {
+            let alert = UIAlertController(title: "Error", message:"All Text Field Must be Filled!", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Ok", style: .default) {(alertAction) in alert.dismiss(animated: true, completion: nil)}
+        }else {
+            performSegue(withIdentifier: "confirmPage", sender: self)
+        }
         
         
         
