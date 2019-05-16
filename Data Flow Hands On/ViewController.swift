d05ac417f4cfcb422c93a0eb70a668bb4359bfd0
Ivan Sebastian <ivansedbert@gmail.com>
2019-05-16 21:27:47 +0700

    initial Commit

--- Data Flow Hands On/ViewController.swift
+++ Data Flow Hands On/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+   
+        
+        
+        
+        
     }
 
 
